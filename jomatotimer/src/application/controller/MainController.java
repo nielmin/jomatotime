ab8fc9f7549fb264bdeccc3d3e63afb40d514043
nielmin <whadmia@gmail.com>
2022-11-23 22:35:54 -0600

    Added basic functionality to the buttons and tweaked GUI.

--- jomatotimer/src/application/controller/MainController.java
+++ jomatotimer/src/application/controller/MainController.java
@@ -1,5 +1,49 @@
 package application.controller;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MainController implements Initializable{
+	@FXML private Label timer;
+	@FXML private Label banner;
+	@FXML private Button pomodoro;
+	@FXML private Button sBreak;
+	@FXML private Button lBreak;
+	@FXML private Button start;
+	@FXML private Button addTask;
 	
+	@FXML private ListView<String> tasks;
+	@FXML private ObservableList<String> taskList;
+	@FXML private TextField task;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		banner.setText("Time to focus!");
+	}
+	
+	public void addTask() {
+		if (task.getText() != "") {
+			tasks.getItems().add(task.getText());
+		}
+	}
+	
+	public void pomodoro() {
+		banner.setText("Time to focus!");
+	}
+	
+	public void shortBreak() {
+		banner.setText("Time for a short break.");
+	}
+	
+	public void longBreak() {
+		banner.setText("Time for a long break!");
+	}
 }
