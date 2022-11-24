894ec1d5f6a63ee434925c3e1ab89c8b9cdbb62c
nielmin <whadmia@gmail.com>
2022-11-23 19:18:40 -0600

    Inital GUI Layout

--- jomatotimer/src/application/Main.java
+++ jomatotimer/src/application/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("view/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
