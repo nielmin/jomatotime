d02c5dcba095609f8ce9a101f3f12c4f2660bbfe
Phillipmoore01 <phillipmoore3102@gmail.com>
2022-11-26 17:10:18 -0600

    Created timer
    Created a timer that counts down through all the predetermined values

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -3,7 +3,11 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,7 +16,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-public class MainController implements Initializable{
+public class MainController implements Initializable, EventHandler<ActionEvent>{
 	@FXML private Label timer;
 	@FXML private Label banner;
 	@FXML private Button pomodoro;
@@ -20,7 +24,7 @@ public class MainController implements Initializable{
 	@FXML private Button lBreak;
 	@FXML private Button start;
 	@FXML private Button addTask;
-	@FXML private Text timerHrs;
+	@FXML private Text timerMin;
 	@FXML private Text timerSec;
 	
 	@FXML private ListView<String> tasks;
@@ -31,8 +35,72 @@ public class MainController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
-		timer.setText("25:00");
+		//timer.setText("25:00");
+		timerMin.setText("25");
+		timerSec.setText("00");
 		banner.setText("Time to focus!");
+	}
+	
+	public void handle(ActionEvent event) {
+		System.out.println("Start Button works");
+		String minutes = timerMin.getText();
+		String seconds = timerSec.getText();
+		
+		
+			try {
+			Thread Timer = new Thread( new Task(){                // put the task in its own thread
+			@Override
+			protected String call() throws Exception {
+		// convert minutes to seconds and while i < total seconds add to the total time
+				int j = Integer.valueOf(seconds) + (Integer.valueOf(minutes)*60);
+				int count = 0;
+				int b = 1;
+				int secTime = 60;
+				String elapsed = String.valueOf(Integer.valueOf(minutes)-1);
+				//while(true) {
+					for(int i = j; i >= 0; i--) {
+						count++;
+					if(count % 60 == 0) 
+					{
+						elapsed = String.valueOf(Integer.valueOf(elapsed)-b);
+						b++;
+						
+						System.out.println(i + " " + b + " " + elapsed + " " + secTime);
+						
+						secTime = 60;
+						
+					}
+					final String fsec = String.valueOf(secTime);
+					secTime--;
+					final String fmin = elapsed;
+					final int counter = count;
+					
+
+					// update the label on the JavaFx Application Thread!
+					Platform.runLater(new Runnable() {
+			            @Override
+			            public void run() {
+			    
+			            	timerMin.setText(fmin);
+			            	
+			            	timerSec.setText(fsec);
+			            	//System.out.println(counter);
+			            }
+			        });
+					Thread.sleep(1000);
+				
+			}
+					return null;
+			}
+		});													
+		
+		// init and run the new thread
+		Timer.setDaemon(true);									
+		Timer.start();
+		Thread.sleep(1000);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+	}
 	}
 	
 	public void addTask() {
@@ -42,23 +110,24 @@ public class MainController implements Initializable{
 	}
 	
 	public void pomodoro() {
-		timer.setText("25:00");
-		timerHrs.setText("25");
+	//	timer.setText("25:00");
+		timerMin.setText("25");
 		timerSec.setText("00");
 		banner.setText("Time to focus!");
 	}
 	
 	public void shortBreak() {
-		timer.setText("05:00");
-		timerHrs.setText("05");
+	//	timer.setText("05:00");
+		timerMin.setText("05");
 		timerSec.setText("00");
 		banner.setText("Time for a short break.");
 	}
 	
 	public void longBreak() {
-		timer.setText("15:00");
-		timerHrs.setText("15");
+	//	timer.setText("15:00");
+		timerMin.setText("15");
 		timerSec.setText("00");
 		banner.setText("Time for a long break!");
 	}
 }
+
