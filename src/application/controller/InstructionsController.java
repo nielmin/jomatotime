55841abdff2a43b9789dac42eb3415a8842e2afc
Phillipmoore01 <phillipmoore3102@gmail.com>
2022-11-26 19:20:43 -0600

    changed the orientation of the views
    starts with instructions then click continue for the main view also redid the fxml loading code for main

--- src/application/controller/InstructionsController.java
+++ src/application/controller/InstructionsController.java
@@ -1,5 +1,32 @@
 package application.controller;
 
-public class InstructionsController {
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class InstructionsController implements EventHandler<ActionEvent>{
+	
+	public void handle(ActionEvent event) {
+		
+		try {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../view/Main.fxml"));
+
+		Scene scene = new Scene((Parent) loader.load());
+
+		Main.stage.setScene(scene);
+		Main.stage.setTitle("Jomato Timer");
+		Main.stage.show();
+								
+		} catch(Exception e) {
+		e.printStackTrace();
+		}
+	}
 
 }
